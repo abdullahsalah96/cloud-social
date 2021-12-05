5ddaeeb78d0ab6326db665aa51039e4b10ca3c13
Abdalla Elshikh <43406571+abdullahsalah96@users.noreply.github.com>
2021-12-05 16:35:00 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import PostsCard from "./Components/PostsCards/PostsCards";
 const Post = require("./Model/Post");
 
 function App() {
-  let posts = [];
-  var d = new Date();
-  const date = d.toISOString().substring(0, 10);    
+  // let posts = [];
+  // var d = new Date();
+  // const date = d.toISOString().substring(0, 10);    
   // const dummyPost = new Post("username", "Random title for post", "Random content for post Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore", date, [{username:"dummyusername", text: "I am commenting a dummy comment to see if it's working Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor"}])
   // const dummyPost2 = new Post("username2", "Random blabla post", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", date, [])
   // posts.push(dummyPost);
